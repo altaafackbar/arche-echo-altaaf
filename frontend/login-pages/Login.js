7c37bc85dd4c1dbe337d115d343e831986d23a48
jasonexus <jasonexus7458@gmail.com>
2021-10-13 09:50:28 -0600

    Pulled from Development and merged my branch

--- frontend/login-pages/Login.js
+++ frontend/login-pages/Login.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { useState } from 'react';
+import { Text, View, Button, StyleSheet, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import CustomInput from '../components/styles/textBox';
+import TextBox from 'react-native-password-eye';
 
 export default function Login ()
 {
-
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const navigation = useNavigation();
 
     function navigateToSignUp () {
@@ -12,13 +16,49 @@ export default function Login ()
     }
 
     return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text> Login Screen </Text>
+    <SafeAreaView style={styles.container}>
+    <View style={styles.headerContainer}>
+    <Text style={styles.headerText}>Welcome Back!</Text>
+    <Text style={styles.subheaderText}>Enter your account details to get started.</Text>
+    </View>
+    <CustomInput placeholder='Email' value={email} setValue={setEmail} label='Email'/>
+    <CustomInput placeholder='Password' value={password} setValue={setPassword} secureTextEntry={true} label='Password'></CustomInput>
     <Button title="Open SignUp Screen"
     onPress= {()=>navigateToSignUp()}
     />
     
-    </View>
+    </SafeAreaView>
     )
 
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#fafafa',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerContainer: {
+        width: '100%',
+        textAlign: 'left',
+        padding: 20,
+        position: 'absolute',
+        right: 5,
+        top: 5,
+    },
+    headerText:{
+        fontSize: 32,
+        color: '#1f1f1f',
+        textAlign: 'left',
+    },
+    subheaderText: {
+        fontSize: 16,
+        color: '#1f1f1f',
+        textAlign: 'left',
+        marginTop: '0.5%',
+        color: '#919191',
+    },
+
+})
